90d9a82f163039076d6437a16b8a105b5d7fd4f2
jacobsamson <72816510+jacobsamson@users.noreply.github.com>
2021-01-12 21:09:52 -0800

    Added game over screen, score counter, and comments.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -4,17 +4,17 @@ import java.awt.event.*;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
-
-    static final int SCREEN_WIDTH = 600;
-    static final int SCREEN_HEIGHT = 600;
+    static final int SCREEN_WIDTH = 500;
+    static final int SCREEN_HEIGHT = 500;
     static final int UNIT_SIZE = 25;
-    static final int GAME_UNITS = (600*600)/25;
+    static final int GAME_UNITS = (SCREEN_HEIGHT * SCREEN_WIDTH)/UNIT_SIZE;
     static final int DELAY = 100;
     //arrays to record x and y coordinates of snake parts
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
     int snakeParts = 6;
     int score = 0;
+    //coordinates of the points/score the snake has to touch
     int pointX;
     int pointY;
     char direction = 'R';
@@ -26,12 +26,14 @@ public class GamePanel extends JPanel implements ActionListener {
         rand = new Random();
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.black);
-        this.setFocusable(true); //JPANEL is focusable by default
+        this.setFocusable(true);
         this.addKeyListener(new myKeyAdapter());
         startGame();
-
     }
 
+    /**
+     * Begins the timer and draws the first point. Sets gameRunning to true.
+     */
     public void startGame(){
         gameRunning = true;
         newPoint();
@@ -42,9 +44,11 @@ public class GamePanel extends JPanel implements ActionListener {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         draw(g);
-
     }
 
+    /**
+     * Draws a grid, the snake, and the points on the screen if gameRunning is true. If false, draws the game over screen.
+     */
     public void draw(Graphics g){
         if(gameRunning) {
             //draws grid on game screen
@@ -100,16 +104,26 @@ public class GamePanel extends JPanel implements ActionListener {
                     g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                 }
             }
+            g.setColor(Color.red);
+            g.setFont(new Font("Sans Serif", Font.BOLD, 20));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Score:" + score, ((SCREEN_WIDTH -  metrics.stringWidth("Score:" + score))/2), g.getFont().getSize());
         }else{
             gameOver(g);
         }
     }
 
+    /**
+     * Creates new points at random locations
+     */
     public void newPoint(){
         pointX = rand.nextInt((int)SCREEN_WIDTH/UNIT_SIZE)*UNIT_SIZE;
         pointY = rand.nextInt((int)SCREEN_HEIGHT/UNIT_SIZE)*UNIT_SIZE;
     }
 
+    /**
+     * Moves the snake by updating the coordinates of each snake part
+     */
     public void move(){
         for(int i = snakeParts - 1; i > 0; i --){
             //System.out.println(i);
@@ -124,6 +138,9 @@ public class GamePanel extends JPanel implements ActionListener {
         }
     }
 
+    /**
+     * Registers if the snake runs into the edge of the screen, a point, or itself
+     */
     public void checkCollision(){
         //checks if snake runs into itself
         for(int i = snakeParts; i > 0; i --){
@@ -145,14 +162,21 @@ public class GamePanel extends JPanel implements ActionListener {
             timer.stop();
     }
 
+    /**
+     * Draws the game over screen
+     */
     public void gameOver(Graphics g){
         g.setColor(Color.red);
         g.setFont(new Font("Sans Serif", Font.BOLD, 75));
         FontMetrics metrics = getFontMetrics(g.getFont());
         g.drawString("Game Over", (SCREEN_WIDTH -  metrics.stringWidth("Game Over"))/2, SCREEN_HEIGHT /2);
+        g.drawString("Score:" + score, ((SCREEN_WIDTH -  metrics.stringWidth("Score:" + score))/2), g.getFont().getSize());
     }
 
     @Override
+    /**
+     * Registers actions then moves the snake, checks for snake collisions, and updates screen.
+     */
     public void actionPerformed(ActionEvent e) {
         if(gameRunning){
             move();
@@ -163,6 +187,11 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public class myKeyAdapter extends KeyAdapter {
         @Override
+        /**
+         * Changes the direction of snake if the player presses the arrow keys or WASD.
+         *
+         * @param e the key pressed
+         */
         public void keyPressed(KeyEvent e){
             switch(e.getKeyCode()){
                 case KeyEvent.VK_LEFT:
